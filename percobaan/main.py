3fe4468aaf9b9bc27a735cf9ae070c1df7d1d65f
hafidzencis <hafidzfebrian21@gmail.com>
2022-02-20 15:01:18 +0700

    hilangkan sambat

--- percobaan/main.py
+++ percobaan/main.py
@@ -5,7 +5,3 @@ print("Coba-4")
 print("Coba-5")
 print("Coba-6")
 print("Coba-7")
-<<<<<<< HEAD
-print("kalo udah direset jgn langsung di push tapi di hilangkan filenya")
-=======
->>>>>>> refs/remotes/origin/master
