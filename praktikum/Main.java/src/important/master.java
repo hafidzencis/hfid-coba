a2c3a970213508141897f7f95f642726784971bb
hafidzencis <hafidzfebrian21@gmail.com>
2022-02-22 13:38:37 +0700

    adding point-6 zz

---
+++ praktikum/Main.java/src/important/master.java
@@ -0,0 +1,13 @@
+package important;
+
+public class master {
+    public static void main(String[] args) {
+
+        System.out.println("Point-1");
+        System.out.println("Point-3");
+        System.out.println("Point-2");
+        System.out.println("Point-4");
+        System.out.println("Point-6 ZZ");
+
+    }
+}
